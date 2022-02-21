f3e85a3b74b0dfb50d775ecacb53742747b4a0fb
Liza <elizabetgrave@mail.ru>
2022-02-21 14:43:40 +0300

    Mistakes fixed

--- MainArrays.java
+++ MainArrays.java
@@ -67,12 +67,13 @@ class MainArrays {
     }
 
 
-    static void returnArray(int len, int initialValue){
+    static int[] returnArray(int len, int initialValue){
         int[] arr = new int[len];
         for (int i = 0; i < len; i++) {
             arr[i] = initialValue;
             System.out.print("[" + i + "]" + arr[i] + " ");
-        }
+        } 
+        return arr;
     }
 }
 
